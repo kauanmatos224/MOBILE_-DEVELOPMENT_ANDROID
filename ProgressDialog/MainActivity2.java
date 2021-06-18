a88badf1bbb7a2a3a1e08f75dbabfbf426a124d6
kauanmatos224 <60532931+kauanmatos224@users.noreply.github.com>
2021-06-18 16:07:34 -0300

    Name Modification

---
+++ ProgressDialog/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.view.View;
+import android.widget.Toast;
+
+//CLASSES IMPORTADAS
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+
+    }
+}
